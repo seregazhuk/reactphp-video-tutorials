1b8d28de790ae25afc633e44ee4e3bab056bcc8d
Zhuk Sergey <seregazhuk88@gmail.com>
2018-10-02 16:07:23 +0300

    Fix code style in sockets chat connections pool

--- socket/ConnectionsPool.php
+++ socket/ConnectionsPool.php
@@ -3,9 +3,9 @@
 use Colors\Color;
 use React\Socket\ConnectionInterface;
 
-class ConnectionsPool {
-    /** @var SplObjectStorage  */
-    protected $connections;
+final class ConnectionsPool
+{
+    private $connections;
 
     public function __construct()
     {
@@ -23,7 +23,7 @@ class ConnectionsPool {
     /**
      * @param ConnectionInterface $connection
      */
-    protected function initEvents(ConnectionInterface $connection)
+    private function initEvents(ConnectionInterface $connection)
     {
         // On receiving the data we loop through other connections
         // from the pool and write this data to them
@@ -39,7 +39,7 @@ class ConnectionsPool {
             $this->sendAll((new Color("$name:"))->bold() ." $data", $connection);
         });
         // When connection closes detach it from the pool
-        $connection->on('close', function() use ($connection){
+        $connection->on('close', function () use ($connection){
             $data = $this->getConnectionData($connection);
             $name = $data['name'] ?? '';
             $this->connections->offsetUnset($connection);
@@ -47,19 +47,19 @@ class ConnectionsPool {
         });
     }
 
-    protected function addNewMember($name, $connection)
+    private function addNewMember(string $name, ConnectionInterface $connection)
     {
         $name = str_replace(["\n", "\r"], '', $name);
         $this->setConnectionData($connection, ['name' => $name]);
         $this->sendAll((new Color("User $name joins the chat\n"))->fg('blue'), $connection);
     }
 
-    protected function setConnectionData(ConnectionInterface $connection, $data)
+    private function setConnectionData(ConnectionInterface $connection, $data)
     {
         $this->connections->offsetSet($connection, $data);
     }
 
-    protected function getConnectionData(ConnectionInterface $connection)
+    private function getConnectionData(ConnectionInterface $connection)
     {
         return $this->connections->offsetGet($connection);
     }
@@ -71,7 +71,7 @@ class ConnectionsPool {
      * @param mixed $data
      * @param ConnectionInterface $except
      */
-    protected function sendAll($data, ConnectionInterface $except) {
+    private function sendAll($data, ConnectionInterface $except) {
         foreach ($this->connections as $conn) {
             if ($conn != $except) {
                 $conn->write($data);
